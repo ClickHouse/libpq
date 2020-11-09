72369a2518634335b48fa9a36f22cfc69765ff46
kssenii <sumarokovakseniia@mail.ru>
2020-11-09 21:23:24 +0300

    Almost buildable

--- include/common/string.h
+++ include/common/string.h
@@ -10,12 +10,15 @@
 #ifndef COMMON_STRING_H
 #define COMMON_STRING_H
 
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 struct StringInfoData;			/* avoid including stringinfo.h here */
 
 /* functions in src/common/string.c */
 extern bool pg_str_endswith(const char *str, const char *end);
-extern int	strtoint(const char *pg_restrict str, char **pg_restrict endptr,
-					 int base);
+extern int	strtoint(const char *str, char **endptr, int base);
 extern void pg_clean_ascii(char *str);
 extern int	pg_strip_crlf(char *str);
 
