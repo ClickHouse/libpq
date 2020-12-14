eb283fe5f9d3e3bfc3ea6bdea17efbf1579865b5
kssenii <sumarokovakseniia@mail.ru>
2020-12-14 21:20:32 +0000

    Update pg_config_os.h

--- configs/pg_config_os.h
+++ configs/pg_config_os.h
@@ -1,15 +1,14 @@
-//if(OS_DARWIN)
-///* src/include/port/darwin.h */
-//#define __darwin__	1
-//
-//#if HAVE_DECL_F_FULLFSYNC		/* not present before macOS 10.3 */
-//#define HAVE_FSYNC_WRITETHROUGH
-//
-//#endif
-//endif()
-//if(OS_LINUX)
-/* src/include/port/linux.h */
+#if defined(OS_DARWIN)
 
+/* src/include/port/darwin.h */
+#define __darwin__	1
+
+#if HAVE_DECL_F_FULLFSYNC		/* not present before macOS 10.3 */
+#define HAVE_FSYNC_WRITETHROUGH
+#endif
+
+#else
+/* src/include/port/linux.h */
 /*
  * As of July 2007, all known versions of the Linux kernel will sometimes
  * return EIDRM for a shmctl() operation when EINVAL is correct (it happens
@@ -30,4 +29,6 @@
  * filesystems, because those don't support O_DIRECT.
  */
 #define PLATFORM_DEFAULT_SYNC_METHOD	SYNC_METHOD_FDATASYNC
-//endif()
+
+#endif
+
